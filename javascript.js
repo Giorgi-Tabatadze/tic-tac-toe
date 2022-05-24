bc7dab78b331f7c8877ebbc563bbd32ec4d53072
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2022-05-24 21:16:21 +0400

    render games and moves
    created module displayController which handless rendering of the dom
    and handles move events.

--- javascript.js
+++ javascript.js
@@ -27,7 +27,7 @@ let gameBoard = function () {
 /*
 gameflow object
 
-private function renderDom that takes gameBoard array and renders squares with
+private function renderDom. using gameBoard array, renders squares with
 event listeners that have unique data-id depending on the array number they are 
 given.
 
@@ -44,8 +44,59 @@ function that creates second player
 
 */
 
+const player = function (name, symbol) {
+  return {name, symbol}
+};
+
+let displayController = function (){
 
 
+  let player1 = player("jack", "X")
+  let player2 = player("bob", "O")
+  let playerTurn = player1;
+
+  const boardContainer = document.querySelector("#board-container");
+
+  const removeAllChildNodes = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+  }
+
+  const renderDom = function () {
+    removeAllChildNodes(boardContainer);
+    gameBoard.grid.forEach(element => {
+      let square = document.createElement("div");
+      square.dataset.id = gameBoard.grid.indexOf(element);
+      square.classList.add("square")
+      if (typeof element === "number"){
+        square.addEventListener("click", moveMade);
+      }
+      else {
+        square.innerHTML = element;
+      }
+      boardContainer.appendChild(square);
+    });
+  };
+
+  const changeTurn = () => {
+    if (playerTurn === player1) {
+      playerTurn = player2;
+    }
+    else {
+      playerTurn = player1;
+    }
+  }
+
+  const moveMade = function(event) {
+    const location = event.target.dataset.id;
+    const symbol = playerTurn.symbol;
+    gameBoard.putSymbol(location, symbol);
+    changeTurn();
+    renderDom();
+  }
+  renderDom();
+}();
 
 
 
