bbfe304bb0f7837165032218e244eb654925f947
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2022-06-02 18:59:02 +0400

    Add New Game Function
    Added function which allows for new games to be started with same
    names or with new names

--- javascript.js
+++ javascript.js
@@ -16,33 +16,39 @@ const removeAllChildNodes = (parent) => {
 }
 };
 
-let gameBoard = function () {
+const gameBoard = function () {
   let grid = [0, 1, 2, 
               3, 4, 5, 
               6, 7, 8];
 
   const putSymbol = function(location, symbol) {
-    grid[location] = symbol;
+    gameBoard.grid[location] = symbol;
     //nextTurn();
   };
 
   const clearBoard = () => {
-    grid = [0, 1, 2, 
-            3, 4, 5, 
-            6, 7, 8];
+    gameBoard.grid = [0, 1, 2, 
+                    3, 4, 5, 
+                    6, 7, 8];
   }
 
   return {grid, putSymbol, clearBoard}
 }();
 
 
-let displayController = function (player1, player2){
+
+const displayController = function (plr1, plr2){
+  //cache players
+  player1 = plr1;
+  player2 = plr2;
 
   let playerTurn = player1;
   let rep = 0;
   let winnerDetermined = false;
+  let message = "";
 
   const boardContainer = document.querySelector("#board-container");
+  const uiContainer = document.querySelector("#ui-container");
 
   const renderBoard = function () {
     removeAllChildNodes(boardContainer);
@@ -78,11 +84,14 @@ let displayController = function (player1, player2){
     rep += 1;
     checkWin ();
     renderBoard();
+    if (winnerDetermined){
+      gameEnd();
+      return;
+    }
     changeTurn();
   };
 
   const checkWin = () => {
-    let message = "";
     if (
       (gameBoard.grid[0] === gameBoard.grid[1] && gameBoard.grid[1] === gameBoard.grid[2]) 
     ||(gameBoard.grid[3] === gameBoard.grid[4] && gameBoard.grid[4] === gameBoard.grid[5])
@@ -95,33 +104,59 @@ let displayController = function (player1, player2){
     ) {
       if (playerTurn === player1) {
         player1.winner = true;
-        console.log(`${player1.name} is the winner`);
+        message = `${player1.name} is the winner`;
         winnerDetermined = true;
         //message
       }
       else {
         player2.winner = true;
-        console.log(`${player2.name} is the winner`)
+        message = `${player2.name} is the winner`
         winnerDetermined = true;
         //message
       }
     } else if (rep === 9){
-      console.log(`its a draw`)
+      message = `its a draw`
       winnerDetermined = true;
       //draw
     };
-  }
-  
-  const playAgain = () => {
-
   };
 
-  const newGame = () => {
+  const clearGame = () => {
+  playerTurn = player1;
+  rep = 0;
+  winnerDetermined = false;
+  message = "";
+  removeAllChildNodes(boardContainer);
+  removeAllChildNodes(uiContainer);
+  };
+  
 
+  const playAgain = () => {
+    clearGame();
+    gameBoard.clearBoard();
+    renderBoard();
+  };
+
+  const newPlayers = () => {
+    location.reload();
   }
 
-  const gameEnd = (message) => {
-    
+  const gameEnd = () => {
+    let messageBoard = document.createElement("h3");
+    messageBoard.innerHTML = message;
+    let playAgainBtn = document.createElement("button")
+    playAgainBtn.setAttribute("id", "play-again");
+    playAgainBtn.setAttribute("type", "button");
+    playAgainBtn.innerText = "Play Again"
+    playAgainBtn.addEventListener("click", playAgain);
+    let newPlayersBtn = document.createElement("button")
+    newPlayersBtn.setAttribute("id", "new-Players");
+    newPlayersBtn.setAttribute("type", "button");
+    newPlayersBtn.innerText = "New Players";
+    newPlayersBtn.addEventListener("click", newPlayers);
+    uiContainer.appendChild(messageBoard);
+    uiContainer.appendChild(playAgainBtn);
+    uiContainer.appendChild(newPlayersBtn);
   }
 
   renderBoard();
@@ -132,46 +167,71 @@ const displayForm = () => {
   let player1;
   let player2;
 
+
   const startGame = () => {
     if (nameInput1.value !== "" && nameInput2.value !== ""){
     let playerName1 = nameInput1.value;
     let playerName2 = nameInput2.value;
     player1 = Player(playerName1, "X");
     player2 = Player(playerName2, "O");
+    removeAllChildNodes(uiContainer);
     displayController(player1, player2);
     };
   };
 
   //cache DOM
-  const boardContainer = document.querySelector("#board-container");
+  const uiContainer = document.querySelector("#ui-container");
   const startForm = document.createElement("form");
   const nameInput1 = document.createElement("input")
+  const nameInput1Label = document.createElement("label")
   const nameInput2 = document.createElement("input")
+  const nameInput2Label = document.createElement("label")
   const startGameButton = document.createElement("button");
 
   //set attributes to dom
   startForm.classList.add("name-form");
+
   nameInput1.setAttribute("type", "text");
+  nameInput1.setAttribute("id", "name-input-1");
+  nameInput1Label.setAttribute("id", "name-input-1");
+  nameInput1Label.innerText = "X Player Name"
   nameInput1.classList.add("name-input");
+
   nameInput2.setAttribute("type", "text");
+  nameInput2.setAttribute("id", "name-input-2");
+  nameInput2Label.setAttribute("id", "name-input-2");
+  nameInput2Label.innerText = "O Player Name"
   nameInput2.classList.add("name-input");
+
   startGameButton.setAttribute("id", "start-game");
   startGameButton.setAttribute("type", "button");
+  startGameButton.innerText = "Start Game"
 
   //add event listeners
   startGameButton.addEventListener("click", startGame)
 
   //createform
+  removeAllChildNodes(uiContainer);
+  startForm.appendChild(nameInput1Label);
   startForm.appendChild(nameInput1);
+  startForm.appendChild(nameInput2Label);
   startForm.appendChild(nameInput2);
   startForm.appendChild(startGameButton);
-  boardContainer.appendChild(startForm);
+  uiContainer.appendChild(startForm);
 
   const Player = (name, symbol) => {
     let winner = false
     return {name, symbol, winner}
-  };  
+  };    
+
 };
+
+const newGame = (() => {
+  const playerVsPlayer = document.getElementById("player-player");
+  const playerVsAi = document.getElementById("player-ai");
+  playerVsPlayer.addEventListener("click", displayForm);
+})();
+
 
 /*
 player object 
